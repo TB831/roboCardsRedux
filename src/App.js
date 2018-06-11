750a7744f2d7856ab7d32bc9dc691a6c896bb6a5
TB831 <druu831@gmail.com>
2018-06-11 16:07:36 -0700

    added validations for robots

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import CardList from './CardList.js';
 import SearchBox from './SearchBox.js';
-import { robots } from './robots.js';
 import './App.css';
 
 class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchField: ''
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((users) => this.setState({robots: users}));
   }
 
   onSearchChange = (event) => {
@@ -22,13 +27,17 @@ class App extends React.Component {
       return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
     });
 
-    return (
-      <div className="tc">
-        <h1 className="f1">RobotFriends</h1>
-        <SearchBox searchChange={this.onSearchChange}/>
-        <CardList robots={filterRobots}/>
-      </div>
-    );
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f1">RobotFriends</h1>
+          <SearchBox searchChange={this.onSearchChange}/>
+          <CardList robots={filterRobots}/>
+        </div>
+      );
+    }
   }
 
 }
