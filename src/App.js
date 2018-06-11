35a9da2810d0bcee571ea1b8bdcc6d252645e2cc
TB831 <druu831@gmail.com>
2018-06-11 12:08:34 -0700

    Added logic to allow filtering of robots

--- src/App.js
+++ src/App.js
@@ -3,14 +3,33 @@ import CardList from './CardList.js';
 import SearchBox from './SearchBox.js';
 import { robots } from './robots.js';
 
-const App = () => {
-  return (
-    <div className="tc">
-      <h1>RobotFriends</h1>
-      <SearchBox/>
-      <CardList robots={robots}/>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: robots,
+      searchField: ''
+    }
+  }
+
+  onSearchChange = (event) => {
+    this.setState({searchField: event.target.value})
+  }
+
+  render() {
+    const filterRobots = this.state.robots.filter(robot => {
+      return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+    });
+
+    return (
+      <div className="tc">
+        <h1>RobotFriends</h1>
+        <SearchBox searchChange={this.onSearchChange}/>
+        <CardList robots={filterRobots}/>
+      </div>
+    );
+  }
+
 }
 
 export default App;
