c8b37ba9ae0f97a07bdfc7f17beac179f0594f82
TB831 <druu831@gmail.com>
2018-11-07 11:50:00 -0800

    Created app 'store'. Provider component now passing down application state.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './containers/App.js';
-import registerServiceWorker from './registerServiceWorker';
-import 'tachyons';
-import { robots } from './robots.js';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import "./index.css";
+import App from "./containers/App.js";
+import registerServiceWorker from "./registerServiceWorker";
+import { searchRobots } from "./reducer";
+import "tachyons";
+
+const store = createStore(searchRobots);
 
 ReactDOM.render(
-  <App robots={robots}/>
-  , document.getElementById('root'));
+<Provider store={store}>
+  <App />
+</Provider>, document.getElementById("root"));
 
 registerServiceWorker();
